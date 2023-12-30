2bcb254d7d256596304d3c2f6ef795ecfd0110dc
leon-schh <118748090+leon-schh@users.noreply.github.com>
2023-12-31 00:16:13 +0100

    Add files via upload

--- SAE3.02/Dump_BDD/serveurbdd.sql
+++ SAE3.02/Dump_BDD/serveurbdd.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.35, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
 --
 -- Host: localhost    Database: serveur
 -- ------------------------------------------------------
--- Server version	8.0.35
+-- Server version	8.0.33
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -62,7 +62,7 @@ CREATE TABLE `client` (
 
 LOCK TABLES `client` WRITE;
 /*!40000 ALTER TABLE `client` DISABLE KEYS */;
-INSERT INTO `client` VALUES ('jnej','jnej','192.168.1.61','no','OFF'),('jouj','jouj','192.168.1.61','no','OFF');
+INSERT INTO `client` VALUES ('test','test','192.168.1.86','no','ON');
 /*!40000 ALTER TABLE `client` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -153,4 +153,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-12-30  2:34:57
+-- Dump completed on 2023-12-31  0:15:26
